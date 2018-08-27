0d588e7c3fd49031b329ee4ccf358627aedf4a46
Jayden Graap <34245723+Graap02@users.noreply.github.com>
2018-08-27 09:36:39 -0500

    made a truck and table with chairs

--- Chapter1_Excercises/src/chapter1_excercises/TablesAndChairs.java
+++ Chapter1_Excercises/src/chapter1_excercises/TablesAndChairs.java
@@ -1,5 +1,37 @@
 package chapter1_excercises;
 
 public class TablesAndChairs {
-
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		//something
+		
+		System.out.println("X                X");
+		System.out.println("X                X");
+		System.out.println("X                X");
+		System.out.println("X                X");
+		System.out.println("X     XXXXXX     X");
+		System.out.println("X     X    X     X");
+		System.out.println("XXXXX X    X XXXXX");
+		System.out.println("X   X        X   X");
+		
+		displayTruck();
+	}
+		
+		
+	public static void displayTruck() {	
+		
+		System.out.println("X         XXXXXXXXXX             X");
+		System.out.println("X        XXXXXXXXXXXX            X");
+		System.out.println("X       XXXXXXXXXXXXXX           X");
+		System.out.println("X       XXXXXXXXXXXXXX           X");
+		System.out.println("X   XXXXXXXXXXXXXXXXXXXXXXXXXXX  X");
+		System.out.println("X   XXXXXXXXXXXXXXXXXXXXXXXXXXX  X");
+		System.out.println("X   XX  X   XXXXXXXXXXXX   X  XX X");
+		System.out.println("X   XX XXX XXXXXXXXXXXXXX XXX XX X");
+		System.out.println("X       X   XXXXXXXXXXXX   X     X");
+	}
+		
+		
 }
