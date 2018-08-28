d6af75d99e51bb6c6f5d71aaab91b5c13930f809
Jayden Graap <34245723+Graap02@users.noreply.github.com>
2018-08-28 09:16:18 -0500

    Created and finished

---
+++ Chapter1_Excercises/src/chapter1_excercises/FavoriteSong.java
@@ -0,0 +1,16 @@
+package chapter1_excercises;
+
+import javax.swing.JOptionPane;
+
+public class FavoriteSong {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		JOptionPane.showMessageDialog(null, "We wake up by the sunrise, and you're staring back at me."
+				+ "I hold onto the moment, as we cast it out to sea."
+				+ "The lights, the lights come on, reflecting off our skin."
+				+ "So burning stars, are all we are, ignited from within.");
+	}
+
+}
