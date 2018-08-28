3341c911057b49ff72279015d6e7242fd65a685e
Jayden Graap <34245723+Graap02@users.noreply.github.com>
2018-08-28 09:34:54 -0500

    Finished

---
+++ Chapter1_Excercises/src/chapter1_excercises/SammysMotto.java
@@ -0,0 +1,13 @@
+package chapter1_excercises;
+
+import javax.swing.JOptionPane;
+
+public class SammysMotto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		JOptionPane.showMessageDialog(null, "Sammy's makes it fun in the sun.");
+	}
+
+}
---
+++ Chapter1_Excercises/src/chapter1_excercises/SammysMotto2.java
@@ -0,0 +1,17 @@
+package chapter1_excercises;
+
+import javax.swing.JOptionPane;
+
+public class SammysMotto2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("sSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsS");
+		System.out.println("Ss Sally's makes it fun in the sun. Ss");
+		System.out.println("sSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsSsS");
+		
+		
+	}
+
+}
