808169cf0032ff5ac015a3aef63e4f30915b0e13
Jayden Graap <34245723+Graap02@users.noreply.github.com>
2018-08-28 09:00:08 -0500

    Completed

--- Chapter1_Excercises/src/chapter1_excercises/MovieQuoteInfo.java
+++ Chapter1_Excercises/src/chapter1_excercises/MovieQuoteInfo.java
@@ -2,12 +2,12 @@ package chapter1_excercises;
 
 import javax.swing.JOptionPane;
 
-public class MovieQuote {
+public class MovieQuoteInfo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-    JOptionPane.showMessageDialog(null, "Mr. Stark I don't feel so good");
+		JOptionPane.showMessageDialog(null, "Mr. Stark, I don't feel so good-Infinity War, Spiderman, 2017");
 	}
 
 }
