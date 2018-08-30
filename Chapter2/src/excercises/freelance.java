66fbb59f8d6512e203a2f2bb579b2c35d8a00b46
Jayden Graap <34245723+Graap02@users.noreply.github.com>
2018-08-30 09:54:25 -0500

    Did more code

---
+++ Chapter2/src/excercises/freelance.java
@@ -0,0 +1,33 @@
+package excercises;
+
+import java.util.Scanner;
+
+public class freelance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		String name;
+		String int1;
+		String int2;
+		String int3;
+		
+		
+		System.out.println("What is your name >> ");
+		name = input.next();
+		System.out.println("What is integer 1? >> ");
+		int1 = input.next();
+		System.out.println("What is integer 2? >> ");
+		int2 = input.next();
+		
+		
+		
+		
+		
+		System.out.println("Your name is " + name);
+		System.out.println(int1 + int2);
+		
+	}
+
+}
